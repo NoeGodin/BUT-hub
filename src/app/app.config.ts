3981987e2a0cc1d55faf90faf52f8ab4a81881d8
Noé Godin <noegodin2@gmail.com>
2025-02-23 02:49:06 +0100

    add : api creds setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, LOCALE_ID, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,17 +6,9 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
-import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';  // Import environment
 
-export const firebaseConfig = {
-  apiKey: process.env['FIREBASE_API_KEY'] as string,
-  authDomain: process.env['FIREBASE_AUTH_DOMAIN'] as string,
-  projectId: process.env['FIREBASE_PROJECT_ID'] as string,
-  storageBucket: process.env['FIREBASE_STORAGE_BUCKET'] as string,
-  messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'] as string,
-  appId: process.env['FIREBASE_APP_ID'] as string,
-  measurementId: process.env['FIREBASE_MEASUREMENT_ID'] as string
-};
+export const firebaseConfig = environment.firebaseConfig;
 
 export const appConfig: ApplicationConfig = {
   providers: [
